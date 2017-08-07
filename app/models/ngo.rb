1ab573c729d9d037b36819d3c07d28874c2014d3
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-07 18:09:08 -0300

    Criei a dependency com projects

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -1,6 +1,6 @@
 class Ngo < ApplicationRecord
   has_many  :volunteers, through: :projects
-  has_many  :projects
+  has_many  :projects, dependent: :destroy
 
   validates :name, :responsible, :password, presence: true, uniqueness: true
   validates :email, presence: true, email: true
