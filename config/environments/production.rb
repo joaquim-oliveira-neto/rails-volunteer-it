8f4a7a83fb954bebaca63bc37b7319ac425625fe
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-07 20:05:01 -0300

    Gem devise instalada e congigurada dentro do app. Navbar alterada para fazer Sign Out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'volunteer-it.herokuapp.com/'}
 end
