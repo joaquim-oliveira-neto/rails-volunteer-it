5a4ae47909916f4f4651fc71fe1c36f595c9bfbc
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-07 17:50:04 -0300

    Validação do project e da ngo

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -1,2 +1,7 @@
 class Ngo < ApplicationRecord
+  has_many  :volunteers, through: :projects
+  has_many  :projects
+
+  validates :name, :responsible, :email, :password, presence: true, uniqueness: true
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+  belongs_to :ngo
+  has_many :volunteers, through: :matches
+
+  validates :name,  presence: true, uniqueness: true
+
 end
