9c585fee19d9a7725e4c38c9190e2230148e63f5
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-07 18:24:12 -0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807193853) do
+ActiveRecord::Schema.define(version: 20170807200604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "matches", force: :cascade do |t|
+    t.integer  "project_id"
+    t.integer  "volunteer_id"
+    t.string   "status"
+    t.text     "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["project_id"], name: "index_matches_on_project_id", using: :btree
+    t.index ["volunteer_id"], name: "index_matches_on_volunteer_id", using: :btree
+  end
+
+  create_table "ngos", force: :cascade do |t|
+    t.string   "name"
+    t.string   "responsible"
+    t.text     "mini_description"
+    t.text     "full_description"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "purpose"
+    t.string   "website"
+    t.string   "facebook"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "mini_description"
+    t.string   "purpose"
+    t.string   "skills"
+    t.boolean  "remote"
+    t.integer  "ong_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
   create_table "volunteers", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "e_mail"
+    t.string   "email"
     t.string   "password"
     t.string   "address"
     t.text     "description"
@@ -26,4 +64,6 @@ ActiveRecord::Schema.define(version: 20170807193853) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "matches", "projects"
+  add_foreign_key "matches", "volunteers"
 end
