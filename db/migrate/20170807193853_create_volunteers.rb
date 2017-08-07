35e4a1825d9dcf9cd363b6e692be871950eeaf7c
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-07 17:01:43 -0300

    Alterada coluna de e_mail para email

--- db/migrate/20170807193853_create_volunteers.rb
+++ db/migrate/20170807193853_create_volunteers.rb
@@ -3,7 +3,7 @@ class CreateVolunteers < ActiveRecord::Migration[5.0]
     create_table :volunteers do |t|
       t.string :first_name
       t.string :last_name
-      t.string :e_mail
+      t.string :email
       t.string :password
       t.string :address
       t.text :description
