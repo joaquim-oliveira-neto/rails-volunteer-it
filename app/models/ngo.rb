587372f75c2a509b4dd11ffedd89c845ba6474cc
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-07 18:07:08 -0300

    Adicionei as validações e as referências

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -2,6 +2,15 @@ class Ngo < ApplicationRecord
   has_many  :volunteers, through: :projects
   has_many  :projects
 
-  validates :name, :responsible, :email, :password, presence: true, uniqueness: true
+  validates :name, :responsible, :password, presence: true, uniqueness: true
+  validates :email, presence: true, email: true
+
+  class EmailValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+        record.errors[attribute] << (options[:message] || "is not an email")
+      end
+    end
+  end
 
 end
