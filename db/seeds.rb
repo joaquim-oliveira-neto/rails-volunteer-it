6fd48e4b980b8fa594f511c772791d2f1e4a011c
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-09 21:57:35 -0300

    Criação de projeto por uma ong implementado. Seed reformulada com e-mial nomes e senhas mais faceis. Css mexido para se colocar wrapper e tirar marging de containner substituindo por padding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ User.destroy_all
 
 Ngo.destroy_all
 
-ngo0 = Ngo.create!(name: "Galt Vestibulares", responsible: "Pedro", mini_description: "Ipsus Lorem, blablabla" , full_description: "Ipsus Lorem, blablabla. Ipsus Lorem, blablabla. Ipsus Lorem, blablabla." , phone: "36819237", address: "Mourato Coelho, 1404, Sao Paulo" , purpose: "Educação", website: "wwww.banana.com.br", facebook: "pedrowski.k", user: User.create(email: "gi.simaes@hotmail.com", password: "35423428"))
-ngo1 = Ngo.create!(name: "Vovó Feliz", responsible: "Rafa" , mini_description: "Ipsus Lorem, blablabla" , full_description: "Ipsus Lorem, blablabla. Ipsus Lorem, blablabla. Ipsus Lorem, blablabla." , phone: "36819237", address: "Mourato Coelho, 1304, Sao Paulo" , purpose: "Idosos", website: "wwww.banana.com.br", facebook: "nani.k", user: User.create(email: "gi.asfadas@hotmail.com", password: "34234528"))
-ngo2 = Ngo.create!(name: "APAE", responsible: "Manoel" , mini_description: "Ipsus Lorem, blablabla" , full_description: "Ipsus Lorem, blablabla. Ipsus Lorem, blablabla. Ipsus Lorem, blablabla." , phone: "36819237", address: "Mourato Coelho, 1204, Sao Paulo" , purpose: "Idosos", website: "wwww.banana.com.br", facebook: "pedroski.k", user: User.create(email: "gi.adsdasd@hotmail.com", password: "3532428"))
+ngo0 = Ngo.create!(name: "Galt Vestibulares", responsible: "Luciano", mini_description: "Ipsus Lorem, blablabla" , full_description: "Ipsus Lorem, blablabla. Ipsus Lorem, blablabla. Ipsus Lorem, blablabla." , phone: "36819237", address: "Mourato Coelho, 1404, Sao Paulo" , purpose: "Educação", website: "wwww.banana.com.br", facebook: "pedrowski.k", user: User.create(email: "ngo1@t.c", password: "123456"))
+ngo1 = Ngo.create!(name: "Vovó Feliz", responsible: "Adriano" , mini_description: "Ipsus Lorem, blablabla" , full_description: "Ipsus Lorem, blablabla. Ipsus Lorem, blablabla. Ipsus Lorem, blablabla." , phone: "36819237", address: "Mourato Coelho, 1304, Sao Paulo" , purpose: "Idosos", website: "wwww.banana.com.br", facebook: "nani.k", user: User.create(email: "ngo2@t.c", password: "123456"))
+ngo2 = Ngo.create!(name: "APAE", responsible: "Manoel" , mini_description: "Ipsus Lorem, blablabla" , full_description: "Ipsus Lorem, blablabla. Ipsus Lorem, blablabla. Ipsus Lorem, blablabla." , phone: "36819237", address: "Mourato Coelho, 1204, Sao Paulo" , purpose: "Idosos", website: "wwww.banana.com.br", facebook: "pedroski.k", user: User.create(email: "ngo3@t.c", password: "123456"))
 
 Project.destroy_all
 
@@ -15,9 +15,9 @@ project2 = Project.create!(name: "Design da Festa", mini_description: "Ipsus Lor
 Volunteer.destroy_all
 
 
-volunteer0 = Volunteer.create!(first_name:"Ropo" , last_name: "Aasdas" , user: User.create( email: "eaef@gmail.com" ,password:"987123"), address:"Mourato, 403" , description:"Banana de Rojanas")
-volunteer1 = Volunteer.create!(first_name:"Ropo" , last_name: "Aasdas", user: User.create(email: "roro@gmail.com" ,password:"124123"), address:"Mourato, 230" , description:"Banana de Bacanas")
-volunteer2 = Volunteer.create!(first_name:"Roro" , last_name: "Baixa", user: User.create(email: "pedro@gmail.com" ,password:"123123"), address:"Mourato, 200" , description:"Banana de Pijamas")
+volunteer0 = Volunteer.create!(first_name:"Joaquim" , last_name: "Oliveira" , user: User.create( email: "vol1@t.c" ,password: "123456"), address:"Mourato, 403" , description:"Banana de Rojanas")
+volunteer1 = Volunteer.create!(first_name:"Marco" , last_name: "Beduschi", user: User.create(email: "vol2@t.c" ,password: "123456"), address:"Mourato, 230" , description:"Banana de Bacanas")
+volunteer2 = Volunteer.create!(first_name:"Rodrigo" , last_name: "Proença", user: User.create(email: "vol3@t.c" ,password: "123456"), address:"Mourato, 200" , description:"Banana de Pijamas")
 
 
 Match.destroy_all
