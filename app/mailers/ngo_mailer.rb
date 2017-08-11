9342e065ea7c3db0080d21e0fed71a530178ec43
marco beduschi <beduschimarco@gmail.com>
2017-08-11 17:43:08 -0300

    Revert "Revert "fotos consertadas""
    This reverts commit 70049aafcc2e18ed50ae7368885ac35222dfb333.

--- app/mailers/ngo_mailer.rb
+++ app/mailers/ngo_mailer.rb
@@ -3,7 +3,6 @@ class NgoMailer < ApplicationMailer
   def welcome(ngo)
     @ngo = ngo  # Instance variable => available in view
     mail(to: @ngo.user.email, subject: 'Seja bem vindo ao VolunteerIT!')
-    # This will render a view in `app/views/user_mailer`!
   end
 
 end
--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -2,7 +2,6 @@ class VolunteerMailer < ApplicationMailer
 
   def welcome(volunteer)
     @volunteer = volunteer  # Instance variable => available in view
-    # This will render a view in `app/views/user_mailer`!
     mail(to: @volunteer.user.email, subject: 'Seja bem vindo ao VolunteerIT!')
   end
 
