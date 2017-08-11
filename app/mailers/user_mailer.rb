e1c2c4c63e309a756beb6900f24ba98844cba3ee
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-11 15:08:40 -0300

    pequenas mudanças no email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user  # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Seja bem vindo ao VolunteerIT!')
     # This will render a view in `app/views/user_mailer`!
   end
 
   def welcome_project(user)
     @user = user  # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Tem um voluntário querend desenvolver o seu projeto!')
+    mail(to: @user.email, subject: 'Um voluntário quer desenvolver o projeto de sua NGO!')
   end
 
 end
