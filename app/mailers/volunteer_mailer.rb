e3bebfeea05788b839a98235defad8a72be233b5
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-11 17:27:48 -0300

    demoday

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -3,7 +3,7 @@ class VolunteerMailer < ApplicationMailer
   def welcome(volunteer)
     @volunteer = volunteer  # Instance variable => available in view
 
-    mail(to: @volunteer.email, subject: 'Seja bem vindo ao VolunteerIT!')
+    mail(to: @volunteer.user.email, subject: 'Seja bem vindo ao VolunteerIT!')
   end
 
 end
